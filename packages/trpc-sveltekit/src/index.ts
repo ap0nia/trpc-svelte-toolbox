053fdf128c5867020ec5ac167ed4a199152dcda0
senti <brianmv@uci.edu>
2023-03-23 08:28:27 -0700

    updated directory

--- packages/trpc-sveltekit/src/index.ts
+++ packages/trpc-sveltekit/src/index.ts
@@ -4,14 +4,9 @@ import type { AnyRouter } from '@trpc/server'
 import type { Handle } from '@sveltejs/kit'
 
 /**
- * All options available to the `createTRPCHandler` function.
- */
-type Options<T extends AnyRouter> = FetchHandlerRequestOptions<T>
-
-/**
  * Create `handle` function for SvelteKit `hooks.server`.
  */
-function createTRPCHandle<T extends AnyRouter>(options: Options<T>): Handle {
+function createTRPCHandle<T extends AnyRouter>(options: FetchHandlerRequestOptions<T>): Handle {
   return async ({ event, resolve }) =>
     event.url.pathname.startsWith(options.endpoint) ? resolve(event) : fetchRequestHandler(options)
 }

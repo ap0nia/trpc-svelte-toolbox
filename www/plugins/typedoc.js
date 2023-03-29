1f67ec33088a94c95774a8eef74b8014e0862848
senti <brianmv@uci.edu>
2023-03-29 16:43:44 -0700

    removed extra stuff

--- www/plugins/typedoc.js
+++ www/plugins/typedoc.js
@@ -2,8 +2,6 @@ function generateTypedocDocusaurusPlugins(directories) {
   return directories.map((directory, idx) => [
     'docusaurus-plugin-typedoc',
     {
-      // TypeDoc options
-      // https://typedoc.org/guides/options/
       id: directory,
       entryPoints: [`../packages/${directory}/src/index.ts`],
       tsconfig: `../packages/${directory}/tsconfig.json`,
@@ -12,16 +10,10 @@ function generateTypedocDocusaurusPlugins(directories) {
       excludeInternal: true,
       excludePrivate: true,
       excludeProtected: true,
-
-      // docusaurus-plugin-typedoc options
-      // https://github.com/tgreyuk/typedoc-plugin-markdown/tree/master/packages/docusaurus-plugin-typedoc#plugin-options
       sidebar: {
         categoryLabel: `@trpc/${directory}`,
         position: idx,
       },
-
-      // Markdown options
-      // https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-content-docs#markdown-front-matter
       frontmatterGlobals: {
         pagination_prev: null,
         pagination_next: null,

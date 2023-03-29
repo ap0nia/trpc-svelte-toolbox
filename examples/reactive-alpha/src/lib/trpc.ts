ae4205a7c4509ef690b03d608b7ccf0f571831e8
senti <brianmv@uci.edu>
2023-03-28 18:55:29 -0700

    removed alpha package

--- examples/reactive-alpha/src/lib/trpc.ts
+++ examples/reactive-alpha/src/lib/trpc.ts
@@ -1,8 +1,7 @@
 import { createTRPCUntypedClient, httpBatchLink } from '@trpc/client'
-import { createTRPCSvelte } from '@bevm0/trpc-svelte-query-alpha'
+import { createTRPCSvelte } from '@bevm0/trpc-svelte-query'
 import { QueryClient } from '@tanstack/svelte-query'
 import type { AppRouter } from './server/trpc/routes'
-import type { AnyQueryProcedure, AnyRouter, inferProcedureInput, inferProcedureOutput } from '@trpc/server'
 
 const url = 'http://localhost:5173/trpc'
 
@@ -15,37 +14,3 @@ export const trpc = createTRPCSvelte<AppRouter>({
 export const client = createTRPCUntypedClient<AppRouter>({
   links: [ httpBatchLink({ url }) ],
 })
-
-
-
-type Mapped<T> = {
-  [k in keyof T]: 
-    T[k] extends AnyRouter ? Mapped<T[k]> :
-    T[k] extends AnyQueryProcedure ? (input: inferProcedureInput<T[k]>) => T[k]
-  : never
-}
-
-type ParseArray<Left extends unknown[], Right extends unknown[] = []> = 
-  Left extends [] ? [] : 
-  Left extends [infer Head] ? [...Right, Head] :
-  Left extends [infer Head, ...infer Tail] ? ParseArray<Tail, [...Right, Head]> :
-  unknown[] extends Left ? Left : []
-
-
-type Results<T extends any[]> = {
-  [k in keyof T]: inferProcedureOutput<T[k]>
-}
-
-type TRPCUseQueries = 
-  <TQueryOptions extends any[]>(callback: (t: Mapped<AppRouter>) => readonly [...ParseArray<TQueryOptions>]) 
-=> Results<TQueryOptions>;
-
-let x: TRPCUseQueries = undefined as any
-
-let y = x((t) => {
-  let a = t.greeting()
-  let b = t['']()
-  return [a, b]
-})
-
-export type Y = typeof y

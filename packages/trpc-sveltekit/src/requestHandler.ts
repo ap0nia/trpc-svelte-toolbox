f229fc25ba1c822a590de3af2b5dc0bc95a44793
senti <brianmv@uci.edu>
2023-03-30 09:01:09 -0700

    added dynamic path evaluation for sveltekit request

--- packages/trpc-sveltekit/src/requestHandler.ts
+++ packages/trpc-sveltekit/src/requestHandler.ts
@@ -3,20 +3,21 @@ import type { AnyRouter } from '@trpc/server'
 import type { RequestHandler } from '@sveltejs/kit'
 import type { TRPCHandleOptions } from './types'
 
-const defaultEndpoint = '/trpc'
-
 /**
  * Create `RequestHandler` function for SvelteKit `+server.ts`, e.g. GET, POST, etc.
  * e.g. Default file location `src/routes/trpc/[...path]/+server.ts`
+ *
+ * @experimental If endpoint isn't provided, it will be inferred from the pathname.
+ * e.g. if pathname is '/api/trpc/,a,b,c', where '/a,b,c' are params, the endpoint will be '/api/trpc'
  */
 export function createTRPCRequestHandler<T extends AnyRouter>(
   options: TRPCHandleOptions<T>
 ): RequestHandler {
-  return async (event) =>
-    await fetchRequestHandler({
+  return async (event) => 
+    fetchRequestHandler({
       ...options,
       req: event.request,
-      endpoint: options.endpoint ?? defaultEndpoint,
+      endpoint: options.endpoint ?? event.url.pathname.substring(0, event.url.pathname.lastIndexOf('/')),
       createContext: (opts) => options?.createContext(opts, event),
     })
 }

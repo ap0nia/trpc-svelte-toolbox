250c87576d9137f3eaacd117f7632fd6d126c10d
senti <brianmv@uci.edu>
2023-03-31 20:23:40 -0700

    cleaned up lib

--- packages/trpc-svelte-query/src/lib/context.ts
+++ packages/trpc-svelte-query/src/lib/context.ts
@@ -1,4 +1,7 @@
-import type { TRPCClientError, TRPCRequestOptions } from '@trpc/client'
+import { setContext, getContext } from 'svelte'
+import { createFlatProxy, createRecursiveProxy } from '@trpc/server/shared'
+import { useQueryClient } from '@tanstack/svelte-query'
+import type { TRPCUntypedClient, TRPCClientError, TRPCRequestOptions } from '@trpc/client'
 import type {
   AnyProcedure,
   AnyQueryProcedure,
@@ -12,6 +15,7 @@ import type {
   InvalidateOptions,
   InfiniteData,
   FetchQueryOptions,
+  FetchInfiniteQueryOptions,
   ResetOptions,
   QueryFilters,
   Updater,
@@ -20,6 +24,7 @@ import type {
   Query,
   RefetchOptions,
 } from '@tanstack/svelte-query'
+import { getQueryKeyInternal } from '$lib/getQueryKey'
 import type { QueryKeyKnown } from '$lib/getQueryKey'
 
 interface TRPCSvelteRequestOptions extends Omit<TRPCRequestOptions, 'signal'> {
@@ -133,3 +138,99 @@ type ContextProcedure<TRouter extends AnyRouter, TProcedure> =
 export type ContextRouter<T extends AnyRouter> = {
   [k in keyof T]: T[k] extends AnyRouter ? ContextRouter<T[k]> : ContextProcedure<T, T[k]>
 } & SharedContext
+
+const TRPC_CONTEXT_KEY = Symbol('TRPC_CONTEXT_KEY')
+
+export function setTRPCContext<T extends AnyRouter>(client: TRPCUntypedClient<T>) {
+  const innerProxy = createRecursiveProxy((options) => {
+    const queryClient = useQueryClient()
+
+    const anyArgs: any = options.args
+
+    const pathCopy = [...options.path]
+
+    const lastArg = pathCopy.pop() ?? ''
+
+    const path = pathCopy.join('.')
+
+    const queryOptions = {
+      queryKey: getQueryKeyInternal(pathCopy, anyArgs[0], 'query'),
+      queryFn: (context) =>
+        client.query(path, anyArgs[0], {
+          ...anyArgs[1],
+          signal: anyArgs[1]?.trpc?.abortOnUnmount ? context.signal : undefined,
+        }),
+      ...anyArgs[1],
+    } satisfies FetchQueryOptions
+
+    const infiniteQueryOptions = {
+      queryKey: getQueryKeyInternal(pathCopy, anyArgs[0], 'infinite'),
+      queryFn: (context) =>
+        client.query(path, anyArgs[0], {
+          ...anyArgs[1],
+          signal: anyArgs[1]?.trpc?.abortOnUnmount ? context.signal : undefined,
+        }),
+      ...anyArgs[1],
+    } satisfies FetchInfiniteQueryOptions
+
+    // general query key used for invalidations, etc.
+    const queryKey = getQueryKeyInternal(pathCopy, anyArgs[0], 'any')
+
+    switch (lastArg) {
+      case 'fetch':
+        return queryClient.fetchQuery(queryOptions)
+
+      case 'prefetch':
+        return queryClient.prefetchQuery(queryOptions)
+
+      case 'getData':
+        return queryClient.getQueryData(queryOptions.queryKey)
+
+      case 'ensureData':
+        return queryClient.ensureQueryData(queryOptions)
+
+      case 'setData':
+        return queryClient.setQueryData(queryOptions.queryKey, anyArgs[0], anyArgs[1])
+
+      case 'fetchInfinite':
+        return queryClient.fetchInfiniteQuery(infiniteQueryOptions)
+
+      case 'prefetchInfinite':
+        return queryClient.prefetchInfiniteQuery(infiniteQueryOptions)
+
+      case 'getInfiniteData':
+        return queryClient.getQueryData(infiniteQueryOptions.queryKey)
+
+      case 'ensureInfiniteData':
+        return queryClient.ensureQueryData(infiniteQueryOptions)
+
+      case 'setInfiniteData':
+        return queryClient.setQueryData(infiniteQueryOptions.queryKey, anyArgs[0], anyArgs[1])
+
+      case 'invalidate':
+        return queryClient.invalidateQueries({ queryKey, ...anyArgs[0] }, anyArgs[1])
+
+      case 'refetch':
+        return queryClient.refetchQueries({ queryKey, ...anyArgs[0] }, anyArgs[1])
+
+      case 'cancel':
+        return queryClient.cancelQueries({ queryKey, ...anyArgs[0] }, anyArgs[1])
+
+      case 'reset':
+        return queryClient.resetQueries({ queryKey, ...anyArgs[0] }, anyArgs[1])
+
+      default:
+        throw new TypeError(`trpc.${path}.${lastArg} is not a function`)
+    }
+  }) as ContextRouter<T>
+
+  const proxy = createFlatProxy<ContextRouter<T>>((initialKey) => {
+    return innerProxy[initialKey]
+  })
+
+  setContext(TRPC_CONTEXT_KEY, proxy)
+}
+
+export function getTRPCContext<T extends AnyRouter>(): ContextRouter<T> {
+  return getContext(TRPC_CONTEXT_KEY)
+}

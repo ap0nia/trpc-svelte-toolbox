98eadda98e1a77d9f02762271101d7c8ae002f17
senti <brianmv@uci.edu>
2023-06-29 07:50:46 -0700

    added example of setting headers

--- examples/kitchen-sink/src/routes/api/trpc/[...path]/+server.ts
+++ examples/kitchen-sink/src/routes/api/trpc/[...path]/+server.ts
@@ -3,11 +3,18 @@ import type { AppRouter } from '$lib/server/trpc/routes'
 import { createTRPCRequestHandler } from '@bevm0/trpc-sveltekit'
 import type { RouteParams, RouteId } from './$types'
 import { createContext } from '$lib/server/trpc/context'
+import type { RequestHandler } from './$types'
 
-const requestHandler = createTRPCRequestHandler<AppRouter, RouteParams, RouteId>({
-  router: appRouter,
-  createContext
-})
+const requestHandler: RequestHandler = event => {
+  event.request.headers.set('x-organization-handle', event.params.path)
+
+  const trpcRequestHandler = createTRPCRequestHandler<AppRouter, RouteParams, RouteId>({
+    router: appRouter,
+    createContext
+  })
+
+  return trpcRequestHandler(event)
+}
 
 export const GET = requestHandler
 export const POST = requestHandler
